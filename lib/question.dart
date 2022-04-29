b7c74e0f33fb3de2c2976d954c0fac67aa421ddc
rogerio-antonio <rgaoliveira@hotmail.com>
2022-04-29 11:15:38 -0300

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,6 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+// https://stackoverflow.com/questions/67034475/non-nullable-instance-field-must-be-initialized
+  Question(this.questionText, this.questionAnswer);
+  }
